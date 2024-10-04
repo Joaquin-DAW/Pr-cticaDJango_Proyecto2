d5b7cf2b0a0415d66587ded1ea93a18f3e5f7581
Joaquin Cabezas Berjillos <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-10-05 01:01:59 +0200

    Actualización final proyecto 2 Django

--- Dinosaurios/urls.py
+++ Dinosaurios/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('Dinosaurios/', views.dinosaurio_list, name="dinosaurio_list"),
     path('Excavaciones/', views.excavacion_list, name="excavacion_list"),
-    path('Paleontogos/', views.paleontologo_list, name="paleontologo_list")
+    path('Paleontologos/', views.paleontologo_list, name="paleontologo_list")
 ]
